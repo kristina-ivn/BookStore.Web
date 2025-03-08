e995bdb4aceb4d992ff3ae023a594fb12247b7d9
kristina-ivn <kris.tina.ivn.07@gmail.com>
2025-03-08 14:41:41 +0200

    Add Models for BookGenre and Constroller for BookGenre

--- BookStore.Web/Data/ApplicationDbContext.cs
+++ BookStore.Web/Data/ApplicationDbContext.cs
@@ -13,5 +13,18 @@ namespace BookStore.Web.Data
         public DbSet<Author> Author { get; set; } = default!;
         public DbSet<Book> Books { get; set; }
         public DbSet<Genre> Genre { get; set; } = default!;
+           public DbSet<BookGenre> BookGenres { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<BookGenre>().HasKey(bg => new { bg.IdGenre, bg.IdBook });
+
+            builder.Entity<Genre>()
+                .HasMany(b => b.Books)
+                .WithMany(g => g.Genres)
+                .UsingEntity(nameof(BookGenre));
+
+            base.OnModelCreating(builder);
+        }
     }
 }
---
+++ BookStore.Web/Data/Entity/BookGenre.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis.Operations;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookStore.Web.Data.Entity
+{
+    public class BookGenre 
+    {
+
+        [ForeignKey(nameof(Book))]
+        public int IdBook { get; set; }
+        public virtual Book? Book { get; set; }
+        [ForeignKey(nameof(Genre))]
+        public int IdGenre { get; set; }
+        public virtual Genre? Genre { get; set; }
+    }
+}
